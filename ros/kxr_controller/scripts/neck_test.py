f443a9f09cbd0c971157a7aa48ee34b778e41c8d
ayaha-n <nagata-ayaha707@g.ecc.u-tokyo.ac.jp>
2024-09-09 19:14:02 +0900

    start writing neck_test.py

--- ros/kxr_controller/scripts/neck_test.py
+++ ros/kxr_controller/scripts/neck_test.py
@@ -3,9 +3,22 @@
 from __future__ import print_function
 
 import argparse
-
+import numpy as np
 import IPython
 from kxr_controller.kxr_interface import KXRROSRobotInterface
 from kxr_models.download_urdf import download_urdf_mesh_files
 import rospy
 from skrobot.model import RobotModel
+
+rospy.init_node('kxr_interface', anonymous=True)
+
+namespace = ''
+download_urdf_mesh_files(namespace)
+
+robot_model = RobotModel()
+robot_model.load_urdf_from_robot_description(
+    namespace + '/robot_description_viz')
+ri = KXRROSRobotInterface(  # NOQA
+    robot_model, namespace=namespace, controller_timeout=60.0)
+
+#define nod, disagree, tilt and then define test
