495103e0052f9060862b9a63d7bab33d9bb1c1fe
ayaha-n <nagata-ayaha707@g.ecc.u-tokyo.ac.jp>
2024-10-22 15:31:15 +0900

    wrote neck-motion part

--- ros/kxr_controller/scripts/neck_test.py
+++ ros/kxr_controller/scripts/neck_test.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+import time
 import argparse
 import numpy as np
 import IPython
@@ -10,7 +11,7 @@ from kxr_models.download_urdf import download_urdf_mesh_files
 import rospy
 from skrobot.model import RobotModel
 
-rospy.init_node('kxr_interface', anonymous=True)
+#rospy.init_node('kxr_interface', anonymous=True)
 
 namespace = ''
 download_urdf_mesh_files(namespace)
@@ -22,3 +23,40 @@ ri = KXRROSRobotInterface(  # NOQA
     robot_model, namespace=namespace, controller_timeout=60.0)
 
 #define nod, disagree, tilt and then define test
+def nod(send_time = 1):
+    robot_model.head_neck_pitch.joint_angle(np.deg2rad(30))
+    ri.angle_vector(robot_model.angle_vector(), send_time)
+    time.sleep(1)
+    robot_model.head_neck_pitch.joint_angle(np.deg2rad(0))
+    ri.angle_vector(robot_model.angle_vector(), send_time)
+
+def disagree(send_time = 1):
+    robot_model.head_neck_yaw.joint_angle(np.deg2rad(30))                                          
+    ri.angle_vector(robot_model.angle_vector(), send_time)                                          
+    time.sleep(1)                                                                                   
+    robot_model.head_neck_yaw.joint_angle(np.deg2rad(-30))
+    ri.angle_vector(robot_model.angle_vector(), send_time)
+    time.sleep(1)                                                                                   
+    robot_model.head_neck_yaw.joint_angle(np.deg2rad(30))
+    ri.angle_vector(robot_model.angle_vector(), send_time)
+    time.sleep(1)                                                                                   
+    robot_model.head_neck_yaw.joint_angle(np.deg2rad(0))                                            
+    ri.angle_vector(robot_model.angle_vector(), send_time)  
+
+def tilt(send_time = 1):
+    robot_model.head_neck_roll.joint_angle(np.deg2rad(30))                                          
+    ri.angle_vector(robot_model.angle_vector(), send_time)
+    time.sleep(2)                                                                                   
+    robot_model.head_neck_roll.joint_angle(np.deg2rad(0))                                           
+    ri.angle_vector(robot_model.angle_vector(), send_time)  
+    
+    
+def test():
+    ri.servo_on()
+    ri.angle_vector(robot_model.init_pose())
+    time.sleep(1)
+    nod()
+    time.sleep(2)
+    disagree()
+    time.sleep(2)
+    tilt()
