00dd2b7a0d9e1b506c1d76b71a0c3bd3c97f5cff
makabe <makabe@jsk.imi.i.u-tokyo.ac.jp>
2024-12-05 20:44:14 +0900

    [kxr_controller] update script

--- ros/kxr_controller/rosbag/pooh_rosbag_record.sh
+++ ros/kxr_controller/rosbag/pooh_rosbag_record.sh
@@ -1,5 +1,7 @@
 # rosbag file is generated in directry where this script is executed.
 # ./pooh_rosbag_record.sh [bag-name]
 # e.g.
-# ./pooh_rosbag_record.sh log-take1
+# $ roscd kxr_controller/rosbag
+# $ rosrun kxr_controller pooh_rosbag_record.sh log-take1
+# take1_20XX-XX-XX-XX-XX-XX.bag is genereted in kxr_controller/rosbag
 rosbag record -o $1 /joint_states /eye_brow/joint_states
